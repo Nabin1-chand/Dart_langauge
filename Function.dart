84cf85ed5b943fe3e1830c4d5ebb0c005a69f98b
Nabin Chand <nbc0117rdx@gmail.com>
2022-12-19 11:41:12 +0545

    function in dart

---
+++ Function.dart
@@ -0,0 +1,16 @@
+// Function in dart=> function is particluar code which is called from any where.. the main objective of function is code resulabilty.
+
+void main() {
+  getUp();
+  takeBreak();
+}
+
+void getUp() {
+  print("get up early moring");
+  print("drink one glass of leak warm water");
+}
+
+void takeBreak() {
+  print("After doing some excerise  you  take breakfast");
+  print("and focus toward your daily work basics schedule");
+}
