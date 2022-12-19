0c04dc1f6a77a4e21114b25a8ac0bab0b9942163
Nabin Chand <nbc0117rdx@gmail.com>
2022-12-19 11:02:12 +0545

    data type and list added in dart

---
+++ DataType.dart
@@ -0,0 +1,18 @@
+void main() {
+  String name = "Nabin";
+  String address = "New Baneshwor";
+  double salary = 150000.00;
+  bool married = false;
+
+  // print(name);
+  // print(id);
+  // print(salary);
+
+  // print(married);
+  // print(langauge);
+
+  print(
+      "My name is ${name}, recently i live in ${address}, and  got ${salary}, as developer and i am familiar with many programming langauge.");
+
+  print(married);
+}
